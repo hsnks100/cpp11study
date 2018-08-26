92f5c028bd7b435c31aebf332ae047fa115c395d
kevin <hsnks100@gmail.com>
2018-08-25 23:57:28 -0400

    c++ pratice

---
+++ ksoo/future-test.cpp
@@ -0,0 +1,69 @@
+#include <thread>
+#include <future>
+#include <iostream>
+ 
+#include <chrono>
+void taskLongTime(std::function<void(void)> callback) {
+    std::thread([callback]() {
+        std::future<int> f = std::async([]()->int {
+            std::this_thread::sleep_for(std::chrono::seconds(3));
+            return 1;
+        });
+
+        f.get();
+        callback(); 
+    }).detach();
+}
+void taskLongTime2(std::function<void(void)> callback) {
+    std::thread([callback]() {
+        std::this_thread::sleep_for(std::chrono::seconds(3));
+        callback(); 
+    }).detach();
+}
+ 
+
+void print_int (std::future<int>& fut) {
+  int x = fut.get();
+  std::cout << "value: " << x << '\n';
+}
+
+void print_int2 (std::promise<int>& pros) {
+  pros.set_value(30);
+}
+int main()
+{
+    taskLongTime([]() {
+        printf("complete\n");
+    });
+    taskLongTime2([]() {
+        printf("complete2\n");
+    });
+    printf("waiting....");
+
+
+
+    {
+        std::promise<int> prom;                      // create promise 
+        std::future<int> fut = prom.get_future();    // engagement with future 
+        std::thread th1 (print_int, std::ref(fut));  // send future to new thread 
+        prom.set_value (10);                         // fulfill promise
+
+        int x = fut.get();
+        std::cout << "value: " << x << '\n';
+        // (synchronizes with getting the future)
+        th1.join();
+    }
+    {
+        std::promise<int> prom;                      // create promise 
+        std::future<int> fut = prom.get_future();    // engagement with future 
+        std::thread th1 (print_int2, std::ref(prom));  // send future to new thread 
+        std::cout << fut.get() << "\n"; 
+        // (synchronizes with getting the future)
+        th1.join();
+    }
+    getchar();
+    //std::future<int> f = std::async(ThreadFunc);
+    //std::cout << f.get() << std::endl;
+ 
+    return 0;
+}
---
+++ ksoo/packaged_task_01.cpp
@@ -0,0 +1,45 @@
+﻿
+#include <iostream>
+#include <thread>
+#include <future>
+#include <utility>
+#include <vector>
+
+
+std::vector<int> GetCharItems(int CharCd)
+{
+	auto ThreadID = std::this_thread::get_id(); 
+	std::cout << "GetCharItems ?????? ID: " << ThreadID << std::endl;
+
+	std::vector<int> itemlist;
+
+	for (int i = 1; i <= 5; ++i) 
+	{
+		itemlist.push_back(i);
+	}
+
+	return itemlist;
+}
+
+int main()
+{
+	auto ThreadID = std::this_thread::get_id(); 
+	std::cout << "???? ?????? ID: " << ThreadID << std::endl;
+
+	std::packaged_task<std::vector<int>(int)> task(&GetCharItems);
+	std::future<std::vector<int>> f = task.get_future();
+
+	std::thread t(std::move(task), 11);
+	t.detach();
+
+
+	auto result = f.get();
+	
+	for(auto ItemCode : result)
+	{
+		std::cout << "ĳ?????? ??????: " << ItemCode << std::endl;
+	}
+  
+	return 0;
+}
+
